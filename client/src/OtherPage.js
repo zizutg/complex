8189ea15050410ae4f3f6f330679cd669c8f2fc8
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-07-16 15:28:18 -0400

    Fib added

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      <h1>Other Page</h1>
+      <p>This is another page in the application.</p>
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+}
