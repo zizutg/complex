89569aa533d1ddaf7e18a6e536304b148ed6b7c6
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-07-17 17:58:19 -0400

    his code

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const OtherPage = () => {
   return (
     <div>
-      <h1>Other Page</h1>
-      <p>This is another page in the application.</p>
-      <Link to="/">Go back to Home</Link>
+      Im some other page!
+      <Link to="/">Go back home</Link>
     </div>
   );
 };
+
 export default OtherPage;
--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,11 @@
 const keys = require('./keys');
 const redis = require('redis');
 
-const client = redis.createClient({
+const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 10000
+  retry_strategy: () => 1000,
 });
-
 const sub = redisClient.duplicate();
 
 function fib(index) {
@@ -17,5 +16,4 @@ function fib(index) {
 sub.on('message', (channel, message) => {
   redisClient.hset('values', message, fib(parseInt(message)));
 });
-
 sub.subscribe('insert');
