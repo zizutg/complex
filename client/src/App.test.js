10261f577e803112c992bf4fbb525d03b81f52d9
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-08-06 20:18:15 -0400

    initiating docker

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,9 +4,22 @@ import axios from 'axios';
 
 jest.mock('axios');
 
-test('renders learn react link', () => {
-  axios.get.mockResolvedValue({ data: {} }); // Mock Axios call
+test('renders learn react link', async () => {
+  // Mock GET /api/values/current
+  axios.get.mockImplementation((url) => {
+    switch (url) {
+      case '/api/values/current':
+        return Promise.resolve({ data: { 1: 1, 2: 1, 3: 2 } });
+      case '/api/values/all':
+        return Promise.resolve({ data: [{ number: 1 }, { number: 2 }, { number: 3 }] });
+      default:
+        return Promise.resolve({ data: {} });
+    }
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  // This is the original test
+  const linkElement = await screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
