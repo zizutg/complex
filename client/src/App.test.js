c10dfd64b481e63c4076ab076138ca47453fbd67
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-08-06 20:16:27 -0400

    initiating docker

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
+
+jest.mock('axios');
 
 test('renders learn react link', () => {
+  axios.get.mockResolvedValue({ data: {} }); // Mock Axios call
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
