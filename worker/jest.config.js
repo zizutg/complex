dc7d47fe61e1ec60fcb59f8214c64c3bfc042746
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-08-06 20:09:19 -0400

    initiating docker

--- worker/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
